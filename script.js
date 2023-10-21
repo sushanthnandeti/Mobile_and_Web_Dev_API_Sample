c8f63817e774c0accee33b9f2c0cb128a7806ad3
Sushanth N <sushanthnandeti96@gmail.com>
2023-10-20 20:14:00 -0400

    API Final Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("https://randomuser.me/api/?results=5").then((data)=>{
+    //console.log(data); 
+    return data.json(); //converted to object
+
+}).then((objectData)=>{
+    console.log(objectData.results);
+
+    let tableData = "";
+    objectData.results.forEach((values)=>{
+        tableData+= 
+        `<tr>
+        <td>${values.name.first}</td>
+        <td>${values.location.city}</td>
+      </tr>`;
+    })
+    document.getElementById("table_body").innerHTML = tableData;
+}).catch((err)=>{
+    console.log(err);
+})
